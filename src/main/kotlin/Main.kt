c1b0ad123b1b76af8073a562ec802964a723a6cb
Mishurin V <mshrvl17@gmail.com>
2023-11-29 21:27:39 +0300

    refactor functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
+import kotlin.math.max
+const val ERROR_CARD = -1
+const val ERROR_LIMIT = -2
 fun main() {
-    val sum = 10000
-    val comission = sum / 100 * 0.75
-    val amount = if(comission > 35) {sum + comission} else {sum + 35}
-println(amount)
+    val sum = comission("Visa", 100, 300)
+    println(sum)
 
 }
+
+fun comission(cardType: String, amount: Int = 0, previous: Int): Int {
+    return when (cardType) {
+        "Visa", "Mir" -> if (amount <= 150000 && amount + previous <= 600000) max(35, (amount * 0.0075).toInt()) else ERROR_LIMIT
+        "MasterCard", "Maestro" -> if (amount + previous <= 75000) amount else (amount + (amount * 0.006) + 20).toInt()
+        else -> ERROR_CARD
+    }
+}
+
