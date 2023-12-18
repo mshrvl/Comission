9e7e32ca362c84b3ba0321c483893384a7a94d79
Mishurin V <mshrvl17@gmail.com>
2023-12-18 20:07:35 +0300

    fix HW

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,41 @@
 import kotlin.math.max
+
 const val ERROR_CARD = -1
 const val ERROR_LIMIT = -2
-fun main() {
-    val sum = comission("VK Pay", 100, 75110)
-    println(sum)
 
+fun main() {
+    val sum = commission("VK Pay", 100, 75110)
+    println(sum)
 }
 
-fun comission(cardType: String, amount: Int = 0, previous: Int): Int {
+fun commission(cardType: String, amount: Int = 0, previous: Int): Int {
     return when (cardType) {
-        "Visa", "Mir" -> if (amount <= 150000 && amount + previous <= 600000) max(35, (amount * 0.0075).toInt()) else ERROR_LIMIT
-        "MasterCard", "Maestro" -> if (amount + previous <= 75000) amount else (amount + (amount * 0.006) + 20).toInt()
-        "VK Pay" -> amount
+        "Visa", "Mir" -> {
+            val totalAmount = amount + previous
+            if (amount <= 150000 && totalAmount <= 600000) {
+                max(35, (amount * 0.0075).toInt())
+            } else {
+                ERROR_LIMIT
+            }
+        }
+        "MasterCard", "Maestro" -> {
+            val totalAmount = amount + previous
+            if (totalAmount <= 75000) {
+                amount
+            } else {
+                (amount + (amount * 0.006) + 20).toInt()
+            }
+        }
+        "VK Pay" -> {
+            val dailyLimit = 15000
+            val monthlyLimit = 40000
+            val totalAmount = amount + previous
+            if (totalAmount <= dailyLimit && totalAmount <= monthlyLimit) {
+                amount
+            } else {
+                ERROR_LIMIT
+            }
+        }
         else -> ERROR_CARD
     }
 }
-
